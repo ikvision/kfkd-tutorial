bd326ccd7f6129b8861577e236fdb8ac9d8d4da2
imryki <imryki@users.noreply.github.com>
2015-09-13 14:19:31 +0300

    Early Stopping in case validation score is nan
    In case the validation score is nan loads the previous best_weights

--- kfkd.py
+++ kfkd.py
@@ -165,7 +165,7 @@ class EarlyStopping(object):
             self.best_valid = current_valid
             self.best_valid_epoch = current_epoch
             self.best_weights = nn.get_all_params_values()
-        elif self.best_valid_epoch + self.patience < current_epoch:
+        elif (self.best_valid_epoch + self.patience < current_epoch) or np.isnan(current_valid):
             print("Early stopping.")
             print("Best valid loss was {:.6f} at epoch {}.".format(
                 self.best_valid, self.best_valid_epoch))
